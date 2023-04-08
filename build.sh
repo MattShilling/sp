817c1653c019928a63a55f38926a142660f62a2f
Matt Shilling <mattthewshilling@gmail.com>
2023-04-08 04:08:08 -0700

    Adding build script.

---
+++ build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Stop on errors.
+set -e
+
+# Format.
+clang-format-11 src/**/*.h src/**/*.cc -i
+
+# Configure the build.
+cmake -B build -G Ninja
+
+# Build the project
+cmake --build build
