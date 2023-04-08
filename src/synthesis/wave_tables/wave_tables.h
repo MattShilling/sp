f639e59bff5724a5fb2651eceb6cd54a98f302a4
Matt Shilling <mattthewshilling@gmail.com>
2023-04-08 04:00:15 -0700

    Adding generated wave tables.

---
+++ src/synthesis/wave_tables/wave_tables.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#include "synthesis/wave_tables/sine_table.h"
+#include "synthesis/wave_tables/square_table.h"
+#include "synthesis/wave_tables/triangle_table.h"
+#include "synthesis/wave_tables/sawtooth_table.h"
